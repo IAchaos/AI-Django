58273ad076f99e629987e70b0b0a59fab9b3d241
IA Chaos <imadmaydo10@gmail.com>
2024-09-09 00:55:02 +0100

    fixing SMTP credentiels leak on main repo

--- Module-01/mysite/mysite/settings.py
+++ Module-01/mysite/mysite/settings.py
@@ -130,5 +130,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'beatmr117@gmail.com'
-EMAIL_HOST_PASSWORD = 'metd obvl lhkt evag'
+EMAIL_HOST_USER = '***********7@gmail.com'
+EMAIL_HOST_PASSWORD = 'ْxxxxx-xxx-xxxx' # almost get leaked
